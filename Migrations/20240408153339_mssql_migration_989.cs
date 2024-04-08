dcb8904ae9ac3c864e4fc0d0ced9fe773874994d
Meri Safaryan <117717996+Merysafaryan@users.noreply.github.com>
2024-04-08 19:48:46 +0400

    Add files via upload

---
+++ Migrations/20240408153339_mssql_migration_989.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Employee.Management.system.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssql_migration_989 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
